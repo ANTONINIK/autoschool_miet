3b3fd459dbc947238a390c4fed2974fe695499f5
IgorKolesov <kolesov2002@list.ru>
2022-10-01 10:38:03 +0300

    2

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './assets/tailwind.css';
 import router from './router/router.js';
-import Math from 'core-js/fn/math';
 
 const app = createApp(App)
 
