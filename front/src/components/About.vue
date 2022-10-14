4abc61ca97246db014354d6c1aa50bf405e0fc2f
IgorKolesov <kolesov2002@list.ru>
2022-10-14 21:04:06 +0300

    news and about

--- front/src/components/About.vue
+++ front/src/components/About.vue
@@ -1,19 +1,91 @@
 <template>
-  <section id="about">
-    <div class="container mx-auto px-5 py-2">
-      <div class="flex justify-between mt-20 mb-20">
-        <div class="flex flex-col w-1/2">
-          <div class="text-darked font-bold text-4xl tracking-right">
-            Заголовок
-          </div>
-          <h3 class="text-xl font-black mt-[2px]">Небольшое описание</h3>
-          <p class="mt-[10px] text-lg">
-            Информация о нас и нашей компании Информация о нас и нашей компании
-            Информация о нас и нашей компании Информация о нас и нашей компании
-          </p>
+  <section class="text-center">
+    <div class="header">
+      <div class="opacity-black">
+        <div class="container">
+          <h1>О нас</h1>
+          <h3>информация</h3>
         </div>
-        <div class="flex flex-col w-[450px]">
-          <img src="../assets/QuestionsImage/10.jpg" alt="about" />
+      </div>
+    </div>
+    <div class="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-10 col-lg-8 ml-auto mr-auto pt-10 pb-10">
+            <h2>Что такое Lamako?</h2>
+            <p>ответ</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="benefits pt-10 pb-10">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h2 class="mb-6">Почему вы должны выбрать нас</h2>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <div class="content-box">
+              <img src="../assets/UsersImage/photo.jpg" alt="">
+              <h3>Преимущество 1</h3>
+              <p>Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание
+                преимущсетва </p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <div class="content-box">
+              <img src="../assets/UsersImage/photo.jpg" alt="">
+              <h3>Преимущество 1</h3>
+              <p>Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание
+                преимущсетва </p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <div class="content-box">
+              <img src="../assets/UsersImage/photo.jpg" alt="">
+              <h3>Преимущество 1</h3>
+              <p>Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание
+                преимущсетва </p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <div class="content-box">
+              <img src="../assets/UsersImage/photo.jpg" alt="">
+              <h3>Преимущество 1</h3>
+              <p>Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание
+                преимущсетва </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="workers pt-10 pb-10">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h2 class="mb-6">Создатели платформы</h2>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="content-box">
+              <img src="../assets/UsersImage/photo.jpg" alt="">
+              <h3>Лаврушкин Юрий</h3>
+              <p>Немного текста, описания, приколов и другой фигни</p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4">
+            <div class="content-box">
+              <img src="../assets/UsersImage/photo.jpg" alt="">
+              <h3>Марьянский Антон</h3>
+              <p>Немного текста, описания, приколов и другой фигни</p>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4 ml-auto mr-auto">
+            <div class="content-box">
+              <img src="../assets/UsersImage/photo.jpg" alt="">
+              <h3>Колесов Игорь</h3>
+              <p>Немного текста, описания, приколов и другой фигни</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -22,8 +94,54 @@
 
 <script>
 export default {
-  name: "About",
+  name: "header",
 };
 </script>
 
-<style></style>
+<style>
+.header {
+  background: url("../assets/UsersImage/car-bg.jpg");
+  background-position: center center;
+  height: 70vh;
+  position: relative;
+}
+
+.opacity-black {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: inherit;
+  z-index: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+}
+
+.opacity-black .container {
+  color: white; 
+  padding-top: 100px;
+}
+
+.benefits{
+  background-color: rgba(0, 0, 0, 0.2);
+}
+.content-box{
+  padding: 0;
+  border-radius: 20px;
+  transition: 0.3s ease-in-out;
+  overflow: hidden;
+  margin-bottom: 20px;
+  background-color: white;
+}
+
+.img{
+  max-width: 100%;
+  overflow: hidden;
+  object-fit: contain;
+  will-change: scale;
+}
+
+@media(hover){
+  .content-box:hover{
+    border: 3px solid #fff;
+  }
+}
+</style>
--- front/src/components/News.vue
+++ front/src/components/News.vue
@@ -1,11 +1,183 @@
 <template>
-  <h5 class="h-screen">News</h5>
+  <section>
+    <div class="header text-center">
+      <div class="opacity-black">
+        <div class="container">
+          <h1>Новости</h1>
+          <h3>какой-нибудь текст</h3>
+        </div>
+      </div>
+    </div>
+    <div class="about text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-10 col-lg-8 ml-auto mr-auto pt-10 pb-10">
+            <h2>Новости Lamako</h2>
+            <p>текст</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="benefits pt-10 pb-10">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <h2 class="mb-6">Новости из мира автомобилей</h2>
+          </div>
+          <div class="col-12">
+            <div class="content-box">
+              <div class="row">
+                <div class="col-12 col-md-6 col-lg-8">
+                  <h3>Новость 1</h3>
+                  <p>Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание
+                    преимущсетва </p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                  <img src="../assets/UsersImage/photo.jpg" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="content-box">
+              <div class="row">
+                <div class="col-12 col-md-6 col-lg-8">
+                  <h3>Новость 1</h3>
+                  <p>Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание
+                    преимущсетва </p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                  <img src="../assets/UsersImage/photo.jpg" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12">
+            <h2 class="mb-6">Новости из мира автоспорта</h2>
+          </div>
+          <div class="col-12">
+            <div class="content-box">
+              <div class="row">
+                <div class="col-12 col-md-6 col-lg-8">
+                  <h3>Новость 1</h3>
+                  <p>Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание
+                    преимущсетва </p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                  <img src="../assets/UsersImage/photo.jpg" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="content-box">
+              <div class="row">
+                <div class="col-12 col-md-6 col-lg-8">
+                  <h3>Новость 1</h3>
+                  <p>Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание
+                    преимущсетва </p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                  <img src="../assets/UsersImage/photo.jpg" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12">
+            <h2 class="mb-6">Новости внутри компании</h2>
+          </div>
+          <div class="col-12">
+            <div class="content-box">
+              <div class="row">
+                <div class="col-12 col-md-6 col-lg-8">
+                  <h3>Новость 1</h3>
+                  <p>Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание
+                    преимущсетва </p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                  <img src="../assets/UsersImage/photo.jpg" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="content-box">
+              <div class="row">
+                <div class="col-12 col-md-6 col-lg-8">
+                  <h3>Новость 1</h3>
+                  <p>Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание преимущсетва Описание
+                    преимущсетва </p>
+                </div>
+                <div class="col-12 col-md-6 col-lg-4">
+                  <img src="../assets/UsersImage/photo.jpg" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    name: "News",
+  name: "News",
 };
 </script>
 
-<style></style>
+<style scoped>
+.header {
+  background: url("../assets/UsersImage/car-bg.jpg");
+  background-position: center center;
+  height: 70vh;
+  position: relative;
+}
+
+.opacity-black {
+  position: absolute;
+  inset: 0;
+  width: 100%;
+  height: inherit;
+  z-index: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+}
+
+.opacity-black .container {
+  padding-top: 100px;
+}
+
+.benefits {
+  background-color: rgba(0, 0, 0, 0.2);
+}
+
+.content-box {
+  border-radius: 20px;
+  transition: 0.3s ease-in-out;
+  will-change: border;
+  overflow: hidden;
+  margin-bottom: 20px;
+  background-color: #fff;
+}
+
+.content-box .row .col-12:first-child{
+  padding-left: 40px;
+  padding-right: 40px;
+  padding-top: 20px;
+}
+
+.img {
+  max-width: 100%;
+  overflow: hidden;
+  object-fit: contain;
+}
+
+@media(hover) {
+  .content-box:hover {
+    border: 3px solid #fff;
+  }
+}
+</style>
+
+
+
